723c96ce074f6612d82f92e22b1256ff360a13ec
luopub <22909077@qq.com>
2026-02-22 18:33:41 +0800

    Auto commit: 周日 2026/02/22 18:33:41

---
+++ push-git.bat
@@ -0,0 +1,49 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+
+echo ========================================
+echo Git Push Script for visualdna_to_listing
+echo ========================================
+echo.
+
+:: Check if remote repositories are configured
+git remote | findstr /x "gitee" >nul
+if errorlevel 1 (
+    echo [INFO] Adding gitee remote...
+    git remote add gitee https://gitee.com/YOUR_USERNAME/visualdna_to_listing.git
+)
+
+git remote | findstr /x "github" >nul
+if errorlevel 1 (
+    echo [INFO] Adding github remote...
+    git remote add github https://github.com/YOUR_USERNAME/visualdna_to_listing.git
+)
+
+:: Get commit message from user or use default
+set /p commit_msg="Enter commit message (press Enter for default): "
+if "%commit_msg%"=="" (
+    set commit_msg=Auto commit: %date% %time:~0,8%
+)
+
+echo.
+echo [1/4] Adding files...
+git add .
+
+echo.
+echo [2/4] Committing changes...
+git commit -m "%commit_msg%"
+
+echo.
+echo [3/4] Pushing to Gitee...
+git push gitee master
+
+echo.
+echo [4/4] Pushing to GitHub...
+git push github master
+
+echo.
+echo ========================================
+echo Done!
+echo ========================================
+pause
